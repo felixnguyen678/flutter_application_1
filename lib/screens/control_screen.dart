b51a699cc932c83d67ba440de4a12aca64264ebb
Nguyen Quang Phuc <phuc.nguyen@snaptec.co>
2023-02-28 15:20:47 +0700

    implement slider in controll screen g

--- lib/screens/control_screen.dart
+++ lib/screens/control_screen.dart
@@ -12,6 +12,7 @@ class ControllScreen extends StatefulWidget {
 }
 
 class _ControllScreenState extends State<ControllScreen> {
+  double sliderValue = 5;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,9 +30,45 @@ class _ControllScreenState extends State<ControllScreen> {
           child: Image.asset(AppAssets.leftArrow),
         ),
       ),
-      body: Container(width: double.infinity, child: Column(children: [
-        
-      ]),),
+      body: Container(
+        width: double.infinity,
+        child: Column(children: [
+          Text(
+            'How much a number word at once',
+            style: AppStyles.h4.copyWith(
+              color: AppColors.blackGrey,
+              fontSize: 18,
+            ),
+          ),
+          Text(
+            '${sliderValue.toInt()}',
+            style: AppStyles.h1.copyWith(
+              fontSize: 150,
+              color: AppColors.blackGrey,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Slider(
+              value: sliderValue,
+              min: 5,
+              max: 100,
+              divisions: 100,
+              onChanged: (value) {
+                setState(() {
+                  sliderValue = value;
+                });
+              }),
+              Text(
+            'Slide to change',
+            style: AppStyles.h1.copyWith(
+              fontSize: 15,
+              color: AppColors.blackGrey,
+              fontWeight: FontWeight.bold,
+              
+            ),
+          ),
+        ]),
+      ),
     );
   }
 }
