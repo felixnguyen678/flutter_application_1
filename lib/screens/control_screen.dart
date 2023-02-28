da39e2aaa6359171a1e840c2fbd9a0f6ba7ba110
Nguyen Quang Phuc <phuc.nguyen@snaptec.co>
2023-02-28 14:27:38 +0700

    implement controll screen's layout

--- lib/screens/control_screen.dart
+++ lib/screens/control_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_application_1/constants/app_assets.dart';
+import 'package:flutter_application_1/constants/app_colors.dart';
+import 'package:flutter_application_1/constants/app_styles.dart';
 
 class ControllScreen extends StatefulWidget {
   const ControllScreen({super.key});
@@ -12,9 +15,23 @@ class _ControllScreenState extends State<ControllScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.white,
+      backgroundColor: AppColors.secondColor,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: AppColors.secondColor,
+        title: Text('Your Control',
+            style: AppStyles.h3
+                .copyWith(color: AppColors.textColor, fontSize: 36)),
+        leading: InkWell(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Image.asset(AppAssets.leftArrow),
+        ),
       ),
+      body: Container(width: double.infinity, child: Column(children: [
+        
+      ]),),
     );
   }
 }
